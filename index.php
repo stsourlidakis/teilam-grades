6fc8ca0ab5e3e39e2d5503ca8a56b4bd4b291b19
John Prantalos <johnprantalos@gmail.com>
2016-06-25 14:45:13 +0300

    Add SEO metas

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+	$url = "http://dev.johnprantalos.pw/teilam-grades";
 	$grades = array(
 		array(
 			'title' => 'Εξάμηνο A',
@@ -279,6 +280,51 @@
 				font-family: 'Cardo', serif;
 			}
 		</style>
+		<!-- Google Meta -->
+	    <link rel="author" href="https://plus.google.com/+JohnPrantalos" />
+	    <link rel="publisher" href="https://plus.google.com/+JohnPrantalos" />
+
+	    <!-- Facebook meta -->
+	    <meta property="og:site_name" content="A JP experiment"/>
+	    <meta property="og:type" content="article" />
+	    <meta property="og:url" content="<?php echo $url ?>" />
+	    <meta property="fb:app_id" content="1469392136712174" />
+	    <meta property="og:locale" content="el_GR" />
+        <meta property="og:image" content="http://i.imgur.com/Q6Db25O.jpg" />
+	    <meta property="og:image:width" content="1920" />
+	    <meta property="og:image:height" content="1080" />
+	    <!-- Twitter Meta -->
+	    <meta name="twitter:site" content="@PandorianGR" />
+	    <meta name="twitter:card" content="A JP experiment" />
+	    <meta name="twitter:url" content="<?php echo $url ?>" />
+        <meta name="twitter:image" content="http://i.imgur.com/Q6Db25O.jpg" />
+
+	    <meta name="description" 
+	    	content="
+	    		PHP script που υπολογίζει τον τελικό Βαθμό Πτυχίου για το ΠΠΣ
+	    		των Μηχανικών Πληροφορικής T.E. του Τ.Ε.Ι. Στερεάς Ελλάδας
+    		"
+		>
+	    <meta property="og:description" 
+	    	content="
+	    		PHP script που υπολογίζει τον τελικό Βαθμό Πτυχίου για το ΠΠΣ
+	    		των Μηχανικών Πληροφορικής T.E. του Τ.Ε.Ι. Στερεάς Ελλάδας
+    		"
+		>
+	    <meta name="twitter:description" 
+	    	content="
+	    		PHP script που υπολογίζει τον τελικό Βαθμό Πτυχίου για το ΠΠΣ
+	    		των Μηχανικών Πληροφορικής T.E. του Τ.Ε.Ι. Στερεάς Ελλάδας
+    		"
+		>
+	    <meta name="author" content="JP" />
+
+	    <meta name="keywords" content="john, prantalos, teiste, teilam, τει, 
+	    	στερεας, ελλαδας, Τ.Ε.Ι., Λαμίας, πληροφορικής" />
+
+	    <meta property="og:title" content="Teilam Grades - JP" />
+	    <meta property="twitter:title" content="Teilam Grades - JP" />
+
 	</head>
 	<body>
 		<div class="container">
