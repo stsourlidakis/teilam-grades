24680b72e51415bec1fcbe6f05b8a7242751004f
John Prantalos <johnprantalos@gmail.com>
2016-06-25 14:31:24 +0300

    First Upload

---
+++ index.php
@@ -0,0 +1,391 @@
+<?php
+	$grades = array(
+		array(
+			'title' => 'Εξάμηνο A',
+			'courses' => array(
+				array(
+					'code' => 'a1',
+					'title' => 'Γραμμική Άλγεβρα',
+					'ects' => 4
+				),
+				array(
+					'code' => 'a2',
+					'title' => 'Μαθηματικά Ι',
+					'ects' => 7
+				),
+				array(
+					'code' => 'a3',
+					'title' => 'Ηλεκτρονική Φυσική',
+					'ects' => 5
+				),
+				array(
+					'code' => 'a4',
+					'title' => 'Φυσική',
+					'ects' => 5
+				),
+				array(
+					'code' => 'a5',
+					'title' => 'Προγραμματισμός Ι',
+					'ects' => 6
+				),
+				array(
+					'code' => 'a6',
+					'title' => 'Εισαγωγή στα Συστήματα Υπολογιστών',
+					'ects' => 3
+				)
+			)		
+		),
+		array(
+			'title' => 'Εξάμηνο B',
+			'courses' => array(
+				array(
+					'code' => 'b1',
+					'title' => 'Μαθηματικά ΙΙ',
+					'ects' => 6
+				),
+				array(
+					'code' => 'b2',
+					'title' => 'Διακριτά Μαθηματικά',
+					'ects' => 3
+				),
+				array(
+					'code' => 'b3',
+					'title' => 'Αρχιτεκτονική Υπολογιστών Ι',
+					'ects' => 5
+				),
+				array(
+					'code' => 'b4',
+					'title' => 'Ψηφιακά Συστήματα Ι',
+					'ects' => 5
+				),
+				array(
+					'code' => 'b5',
+					'title' => 'Προγραμματισμός ΙΙ',
+					'ects' => 6
+				),
+				array(
+					'code' => 'b6',
+					'title' => 'Δομές Δεδομένων',
+					'ects' => 5
+				)
+			)		
+		),
+		array(
+			'title' => 'Εξάμηνο Γ',
+			'courses' => array(
+				array(
+					'code' => 'c1',
+					'title' => 'Αρχιτεκτονική Υπολογιστών ΙΙ',
+					'ects' => 5
+				),
+				array(
+					'code' => 'c2',
+					'title' => 'Πιθανότητες-Στατιστική',
+					'ects' => 5
+				),
+				array(
+					'code' => 'c3',
+					'title' => 'Αγγλική Ορολογία Πληροφορικής',
+					'ects' => 3
+				),
+				array(
+					'code' => 'c4',
+					'title' => 'Ψηφιακά Συστήματα ΙΙ',
+					'ects' => 6
+				),
+				array(
+					'code' => 'c5',
+					'title' => 'Σχεδιασμός και Ανάλυση Αλγορίθμων',
+					'ects' => 6
+				),
+				array(
+					'code' => 'c6',
+					'title' => 'Προγραμματισμός ΙΙΙ',
+					'ects' => 5
+				)
+			)		
+		),
+		array(
+			'title' => 'Εξάμηνο Δ',
+			'courses' => array(
+				array(
+					'code' => 'd1',
+					'title' => 'Λειτουργικά Συστήματα',
+					'ects' => 6
+				),
+				array(
+					'code' => 'd2',
+					'title' => 'Γραμμικά Συστήματα',
+					'ects' => 4
+				),
+				array(
+					'code' => 'd3',
+					'title' => 'Βάσεις Δεδομένων',
+					'ects' => 6
+				),
+				array(
+					'code' => 'd4',
+					'title' => 'Δίκτυα Δεδομένων Ι',
+					'ects' => 6
+				),
+				array(
+					'code' => 'd5',
+					'title' => 'Εισαγωγή στη Φιλοσοφία',
+					'ects' => 3
+				),
+				array(
+					'code' => 'd6',
+					'title' => 'Επιχειρισιακή Έρευνα',
+					'ects' => 5
+				)
+			)		
+		),
+		array(
+			'title' => 'Εξάμηνο Ε',
+			'courses' => array(
+				array(
+					'code' => 'e1',
+					'title' => 'Τεχνητή Νοημοσύνη',
+					'ects' => 5
+				),
+				array(
+					'code' => 'e2',
+					'title' => 'Μικροεπεξεργαστές',
+					'ects' => 6
+				),
+				array(
+					'code' => 'e3',
+					'title' => 'Τεχνολογία Λογισμικού',
+					'ects' => 6
+				),
+				array(
+					'code' => 'e4',
+					'title' => 'Ψ.Ε.Σ.',
+					'ects' => 6
+				),
+				array(
+					'code' => 'e5',
+					'title' => 'Δίκτυα Δεδομένων ΙΙ',
+					'ects' => 7
+				)
+			)		
+		),
+		array(
+			'title' => 'Εξάμηνο ΣΤ',
+			'courses' => array(
+				array(
+					'code' => 'f1',
+					'title' => 'Κατανεμημένα Συστήματα',
+					'ects' => 6
+				),
+				array(
+					'code' => 'f2',
+					'title' => 'Ασφάλεια και προστασία Δεδομένων',
+					'ects' => 6
+				),
+				array(
+					'code' => 'f3',
+					'title' => 'Τηλεπικοινωνίες',
+					'ects' => 6
+				),
+				array(
+					'code' => 'f4',
+					'title' => 'Σ.Α.Ε.',
+					'ects' => 6
+				),
+				array(
+					'code' => 'f5',
+					'title' => 'Αυτόματα ή Ολοκληρωμένα Κυκλώματα',
+					'ects' => 6
+				)
+			)		
+		),
+		array(
+			'title' => 'Εξάμηνο Ζ',
+			'courses' => array(
+				array(
+					'code' => 'g1',
+					'title' => 'Σχεδίαση Δικτύων',
+					'ects' => 6
+				),
+				array(
+					'code' => 'g2',
+					'title' => 'Ηλεκτρονικό Εμπόριο',
+					'ects' => 6
+				),
+				array(
+					'code' => 'g3',
+					'title' => 'Ψ.Ε.Ε.',
+					'ects' => 6
+				),
+				array(
+					'code' => 'g4',
+					'title' => 'Ειδικά Θέματα Κ.Σ.',
+					'ects' => 6
+				),
+				array(
+					'code' => 'g5',
+					'title' => 'Πληροφοριακά Συστήματα ή Πληροφορική στην Εκπαίδευση',
+					'ects' => 6
+				)
+			),
+		),	
+		array(
+			'title' => 'Εξάμηνο Η',
+			'courses' => array(
+				array(
+					'code' => 'h1',
+					'title' => 'Πτυχιακή',
+					'ects' => 20
+				)
+			)		
+		)
+	);
+
+	if ($_GET) {
+		$totalEcts = 0;
+		$totalGrades = 0;
+		$totalCourses = 0;
+		foreach ($grades as $semester) {
+			foreach ($semester['courses'] as $course) {
+				if (!empty($_GET[$course['code']])) {
+					$totalGrades += $_GET[$course['code']] * $course['ects'];
+					$totalEcts += $course['ects'];
+					$totalCourses ++;
+				}
+			}
+		}
+		$grade = $totalGrades / $totalEcts;
+		$grade = round($grade,2);
+		echo $grade;
+
+	}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Teilam Grades - JP</title>
+		<link 
+			href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" 
+			rel="stylesheet" 
+			integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" 
+			crossorigin="anonymous">
+		<link 
+			href='https://fonts.googleapis.com/css?family=Cardo&subset=greek,latin' 
+			rel='stylesheet' 
+			type='text/css'>
+		<style type="text/css">
+			html,body{
+				font-family: 'Cardo', serif;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="container">
+			<div class=row>
+			 	<div class="col-md-6">
+			 		<div class="jumbotron">
+			 			<h2>Υπολογισμός Βαθμού Πτυχίου</h2>
+			 			<p>
+			 				Ένα απλό script που σας δίνει την δυνατότητα να
+			 				βάλετε βαθμούς και στα μαθήματα που δεν έχετε
+			 				περάσει ακόμη, για να πάρετε μια ιδέα της βαρύτητας
+			 				της βαθμολογίας των μαθημάτων που σας απομένουν.
+		 				</p>
+		 				<p>
+		 					Για υποδιαστολή χρησιμοποιείτε . και όχι ,
+		 				</p>
+		 				<p>
+		 					Τα μαθήματα με κενή βαθμολογία δεν λαμβάνονται υποψη
+		 					στον υπολογισμό, οπότε μπορείτε να αφήνετε κενές
+		 					βαθμολογίες.
+		 				</p>
+			 		</div>
+			 	</div>
+				<?php if($_GET): ?>
+					<div class="col-md-6">
+						<div class="jumbotron">
+							<h2>Βαθμός Πτυχίου</h2>
+							<p>
+								Λαμβάνοντας υπόψη βαθμολογία για 
+								<?php echo $totalCourses; ?> μαθήματα από τα 40
+								(μαζί με την πτυχιακή) ο τελικός βαθμός πτυχίου
+								σας διαμορφώνεται στις <?php echo $grade ?>
+								μονάδες.	
+							</p>
+							<p>
+								Ο βαθμός βγαίνει από τον τύπο:
+								<img class="img-responsive" 
+									src="http://i.imgur.com/vgsoC7d.jpg">
+								όπου β1, β2, ... , βν είναι οι βαθμοί των 
+								μαθημάτων και της πτυχιακής και δ1, δ2, ..., δν 
+								οι αντίστοιχες ETCS μονάδες.
+							</p>
+						</div>
+					</div>
+
+				<?php endif; ?>	
+			</div>
+			<div class="row">
+				<form method="GET">
+					<div class="row">
+						<table class="table table-bordered">
+							<thead>
+								<tr>
+									<th class="text-center">Μάθημα</th>
+									<th class="text-center">ECTS</th>
+									<th class="text-center">Βαθμός</th>
+								</tr>
+							</thead>
+							<tbody>
+								<?php foreach ($grades as $semester): ?>
+									<tr>
+										<th colspan="3" class="text-center">
+											<?php echo $semester['title']; ?>
+										</th>
+									</tr>
+									<?php foreach ($semester['courses'] as $course): ?>
+										<tr>
+											<td>
+												<?php echo $course['title']; ?>
+											</td>
+											<td width="20px">
+												<?php echo $course['ects']; ?>
+											</td>
+											<td width="80px">
+												<input class="form-control input-sm" 
+													type="text" 
+													name="<?php echo $course['code']; ?>"
+													value="<?php
+														$code = $course['code'];
+														if (isset($_GET[$code])){
+															echo $_GET[$code];
+														} 
+													?>"
+													min="0" max="10">
+											</td>
+										</tr>
+									<?php endforeach; ?>
+								<?php endforeach; ?>
+							</tbody>
+						</table>
+					</div>
+					<div class="row">
+						<div class="col-md-4 col-md-offset-8">
+							<input class="btn btn-success btn-lg btn-block" 
+								type="submit" value="Υπολογισμός">
+						</div>
+					</div>
+					
+				</form>
+			</div>
+			<div class="row text-center">
+				Διαθέσιμος ο Source Code στο 
+				<a href="https://github.com/JohnPrantalos/teilam-grades">Github
+				</a>.
+			</div>
+		</div>
+		
+	</body>
+</html>
